51bad53154d10939c0d4c8b7046a80b169e598a1
k0m1 <115169534+k0m11@users.noreply.github.com>
2023-01-28 20:57:46 +0300

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,47 @@
+let Questions = []
+let que = document.querySelector("#que")
+let ans = document.querySelector("#ans")
+let create = document.querySelector("#create")
+let local = JSON.parse(localStorage.getItem("Questions"))
+let listQue = document.querySelector("#listQue")
+let delBtn = document.querySelector("#deleteAll")
+let delLastBtn = document.querySelector("#deleteLast")
+
+if (local){
+    Questions = local
+    Render()
+}
+
+create.addEventListener("click", function(){
+    Questions.push(`${que.value} ==> ${ans.value}`)
+    que.value = ""
+    ans.value = ""
+    localStorage.setItem('Questions', JSON.stringify(Questions))
+    Render()
+})
+
+function Render(){
+    let list = ""
+    for (i = 0; i < Questions.length; i++){
+        list += `
+        <li>
+            ${Questions[i]}
+        </li>`
+    }
+    listQue.innerHTML = list
+}
+
+delBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    Questions = []
+    Render()
+})
+
+delLastBtn.addEventListener("dblclick", function(){
+    localStorage.Questions
+    localStorage.Questions = JSON.stringify(Questions.slice(0, -1))
+    Questions.pop()
+    console.log(Questions)
+    Render()
+})
+
