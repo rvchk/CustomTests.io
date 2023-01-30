fc0c0d0287d07b7bade323c717560e9f447592f5
k0m1 <115169534+k0m11@users.noreply.github.com>
2023-01-30 16:26:33 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,21 @@
 // Create a Question
+let QuestionsTest = []
 let Questions = []
 let que = document.querySelector("#que")
 let ans = document.querySelector("#ans")
 let create = document.querySelector("#create")
 let local = JSON.parse(localStorage.getItem("Questions"))
+let localTest = JSON.parse(localStorage.getItem("QuestionsTest"))
 let listQue = document.querySelector("#listQue")
 let delBtn = document.querySelector("#deleteAll")
 let delLastBtn = document.querySelector("#deleteLast")
 
 if (local){
     Questions = local
+    QuestionsTest = localTest
     Render()
 }
+
 
 create.addEventListener("click", function(){
     if (que.value === "" && ans.value === ""){
@@ -19,10 +23,13 @@ create.addEventListener("click", function(){
     }
     else {
         Questions.push(`${que.value} ==> ${ans.value}`)
+        QuestionsTest.push(`${que.value} ==> `)
         que.value = ""
         ans.value = ""
         localStorage.setItem('Questions', JSON.stringify(Questions))
+        localStorage.setItem("QuestionsTest", JSON.stringify(QuestionsTest))
         Render()
+        renderTest()
     }
 })
 
@@ -41,6 +48,7 @@ delBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     Questions = []
     Render()
+    renderTest()
 })
 
 delLastBtn.addEventListener("dblclick", function(){
@@ -49,6 +57,31 @@ delLastBtn.addEventListener("dblclick", function(){
     Questions.pop()
     console.log(Questions)
     Render()
+    renderTest()
 })
 
 // Test
+let test = document.querySelector("#test")
+let testQue = document.querySelector("#testQue")
+let testAns = document.querySelector("#testAns")
+let testBtn = document.querySelector("#testBtn")
+let testInd = 0
+console.log(localTest)
+
+function renderTest(){
+    test.innerHTML = `TEST ${testInd}/${Questions.length}`
+}
+
+testBtn.addEventListener("click", function(){
+    testAns.value = ""
+    if (testInd < Questions.length){
+        testInd += 1
+    }
+    if(testInd == Questions.length){
+        testInd --
+    }
+    testQue.innerHTML = QuestionsTest[0]
+    QuestionsTest = QuestionsTest.slice(1)
+    renderTest()
+    console.log(QuestionsTest)
+})
