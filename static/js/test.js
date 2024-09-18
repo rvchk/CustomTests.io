08dd3d9fadfb1de344242cc9448aa366392faf52
rvchk <115169534+k0m11@users.noreply.github.com>
2024-09-18 10:07:09 +0300

    Update test.js
    Making code smaller

--- static/js/test.js
+++ static/js/test.js
@@ -6,40 +6,37 @@ let ansTheQue = document.querySelector("#ansTheQue")
 let testInd = 0
 let RightAns = 0
 testAns.style.display = "none"
+let QueLength = Questions.length-1
 
 renderTest = () => {
-    test.innerHTML = `TEST ${testInd}/${Questions.length}`
-    if (!Answers.length){
-        testQue.innerHTML = `Result is ${RightAns}/${Questions.length}`
+    test.innerHTML = `TEST ${testInd}/${QueLength}`
+    if (Questions.length == 0){
+        testQue.innerHTML = `Result is ${RightAns}/${QueLength}`
         testAns.style.display = "none"
         testBtn.innerHTML = "START"
         ansTheQue.innerHTML = ""
-        QuestionsTest = localTest
-        Answers = localAns
         testInd = 0
         RightAns = 0
-        renderTest()
     }
 }
 
 testBtn.addEventListener("click", function(){
-    testQue.innerHTML = QuestionsTest[0]
     ansTheQue.innerHTML = "Answer the Question"
     testAns.style.display = "block"
     testBtn.innerHTML = "Answer"
-    if (!Questions.length){
-        QueStart = false
-        renderTest()
-    }
-    if(testAns.value == Answers[0]){
+    console.log(Questions.length)
+
+    if (testAns.value == Questions[0].split(" ")[2]){
+        console.log("Correct")
         RightAns += 1
     }
     testAns.value = ""
     if (testInd < Questions.length){
         testInd += 1
     }
-    testQue.innerHTML = QuestionsTest[0]
-    QuestionsTest = QuestionsTest.slice(1)
-    Answers = Answers.slice(1)
+    if (Questions.length > 1) {
+        testQue.innerHTML = Questions[1].split(" ").slice(0,2).join(" ")
+    }
+    Questions = Questions.slice(1)
     renderTest()
 })
