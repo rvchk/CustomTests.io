4973670a179e1ee08dd5f43a058e9f2acbe58d60
MatveiRovchak <matvejrovcak@gmail.com>
2024-10-10 21:50:16 +0300

    Last changes

--- static/js/test.js
+++ static/js/test.js
@@ -36,6 +36,14 @@ function NoQuestions() {
     testContainer.style = "box-shadow: 0px 0px 0px 0px; margin-top: 0px; width: auto;"
 }
 
+function AllToDefault() {
+    clearInterval(testTime)
+    TestTimerUI.innerHTML = 0
+    questionIndex = 0
+    rightAnswers = 0
+    questions = localQuestions
+}
+
 function RenderUI(option) {
     if (option) {
         testQuestion.innerHTML = `Result is ${rightAnswers}/${questionsLength}`
@@ -56,15 +64,11 @@ function Render() {
     testLengthUI.innerHTML = questionsLength
 
     if (questions.length == 0){
-        testTimers.push(timer)
-        clearInterval(testTime)
-        TestTimerUI.innerHTML = 0
+        AllToDefault()
 
+        testTimers.push(timer)
         RenderUI(true)
         completedTimes++
-        questionIndex = 0
-        rightAnswers = 0
-        questions = localQuestions
         localStorage.setItem('completedTimes', JSON.stringify(completedTimes))
         localStorage.setItem('testTimers', JSON.stringify(testTimers))
     }
@@ -73,7 +77,7 @@ function Render() {
 function Test() {
     // Timer
     if (testButton.innerHTML == "Start") {
-        timer = 55
+        timer = 0
         testTime = setInterval(() => {
             timer++
             TestTimerUI.innerHTML = timer
@@ -103,69 +107,3 @@ function Test() {
 
 testButton.addEventListener("click", Test)
 Render()
-
-/* let TestTimers = []
-let CompletedTimes = 0 
-let localTimer = JSON.parse(localStorage.getItem("TestTimers"))
-let localCompleted = JSON.parse(localStorage.getItem("CompletedTimes")) */
-
-/* if (localCompleted)  {
-    CompletedTimes = localCompleted
-    TestTimers = localTimer
-}
-
-if (questions.length == 0) {
-    document.querySelector(".test").innerHTML = `
-        <div class="List-Error-block">
-            <h1>Oops...</h1>
-            <img src="static/imgs/Error-icon.png" alt="Error-icon">
-            <h2>You Don't have any Questions yet</h2>
-            <h2 style="margin-top: 70px; margin-bottom: 30px;">Go and create one</h2>
-            <a href="index.html">
-                <button>Create</button>
-            </a>
-        </div>`
-}
-
-renderTest = () => {
-    test.innerHTML = `TEST ${testInd}/${queLength}`
-    if (questions.length == 0){
-        timer == null ? timer = 0: TestTimers.push(timer)
-        timer = 0
-        clearInterval(time)
-        time = setInterval(() => {
-            timer++
-        }, 1000);
-
-        testQue.innerHTML = `Result is ${RightAns}/${queLength}`
-        testAns.style.display = "none"
-        testBtn.innerHTML = "START"
-        ansTheQue.innerHTML = ""
-        testInd = 0
-        RightAns = 0
-        CompletedTimes++
-        localStorage.setItem('CompletedTimes', JSON.stringify(CompletedTimes))
-        localStorage.setItem('TestTimers', JSON.stringify(TestTimers))
-    }
-}
-
-testBtn.addEventListener("click", function(){
-    if (testAns.value == questions[0].Answer){
-        console.log("Correct")
-        RightAns += 1
-    }
-    testAns.value = ""
-    if (testInd < queLength){
-        testInd ++
-    }
-    if (testBtn.innerHTML == "Answer") {
-        questions = questions.slice(1)
-    }
-    if (questions.length >= 1) {
-        testQue.innerHTML = questions[0].Question
-    }
-    ansTheQue.innerHTML = "Answer the Question"
-    testAns.style.display = "block"
-    testBtn.innerHTML = "Answer"
-    renderTest()
-}) */
