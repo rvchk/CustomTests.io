3d328e13e0cdd9e0fd9e7b24941e1c8b43b60c2b
MatveiRovchak <matvejrovcak@gmail.com>
2024-10-13 21:32:44 +0300

    Fixed bug with deleting all questions

--- static/js/index.js
+++ static/js/index.js
@@ -11,10 +11,8 @@ const checkBoxUI = document.querySelector("#ButtonsCheck")
 const questionsUI = document.querySelector(".Questions-list")
 
 let localQuestions = JSON.parse(localStorage.getItem("localQuestions"))
-let localCompletedTests = JSON.parse(localStorage.getItem("CompletedTimes"))
-let localTimeArray = JSON.parse(localStorage.getItem("TestTimers"))
 let completed = JSON.parse(localStorage.getItem("completedTimes"))
-let timers = JSON.parse(localStorage.getItem("testTimers"))
+let localTimeTests = JSON.parse(localStorage.getItem("testTimers"))
 
 if (localQuestions) {
     questions = localQuestions
@@ -59,8 +57,14 @@ function Render() {
         })
 
     if (questions.length == 0) {
-        completedTimes = 0
-        localStorage.setItem('completedTimes', JSON.stringify(completedTimes))
+        completed = 0
+        localTimeTests = []
+        localStorage.setItem('testTimers', JSON.stringify(localTimeTests))
+        localStorage.setItem('completedTimes', JSON.stringify(completed))
+
+        completedTestsUI.innerHTML = completed
+        timeArrayUI.innerHTML = 0
+
 
         questionsUI.innerHTML = `
             <div class="Error-block">
@@ -79,7 +83,7 @@ function Render() {
           
     questionsLengthUI.innerHTML = questions.length
     completedTestsUI.innerHTML = completed
-    timeArrayUI.innerHTML = Math.round(timers.reduce((x,y)=>x+y)/timers.length)
+    timeArrayUI.innerHTML = localTimeTests ? Math.round(localTimeTests.reduce((x,y)=>x+y)/localTimeTests.length): 0
         
     let allDeleteButtons = document.querySelectorAll("#deleteButton")
     allDeleteButtons.forEach(deleteButton => deleteButton.addEventListener("click", DeleteQuestion)) 
@@ -95,92 +99,3 @@ function DeleteQuestion() {
 
 createButton.addEventListener("click", CreateQuestion)
 Render()
-/* create.addEventListener("click", () => {
-    // Check || If All Fields are Empty
-    if (questionInput.value === "" || answerInput.value === ""){
-        return
-    }
-    else {
-        const date = new Date()
-        let CurrentDate = `${String(date).split(" ").slice(1,4).join("/")}`
-        // Pushing all data to an array
-
-        if (CheckBox.checked) {
-            Questions.push(`${questionInput.value} ==> ${answerInput.value} Button ${CurrentDate}`)
-        }
-        else {
-            Questions.push(`${questionInput.value} ==> ${answerInput.value} Text ${CurrentDate}`)
-        }
-
-        // Add all Parameters in Arrays
-        // Get Zero Inf in Input Field
-        questionInput.value = ""
-        answerInput.value = ""
-        // Set all Arrays in LocalStorage
-        localStorage.setItem('Questions', JSON.stringify(Questions))
-        Render()
-        renderTest()
-    }
-}) */
-//
-
-// This Fucntion Shows all Changes on the Page
-/* function Render() {
-    if (Questions.length > 1) {
-        ListBlockUI.innerHTML = `
-            <h1>List of Questions</h1>
-            <ul id="listQue"></ul>
-        `
-    }
-
-    lengthUI.innerHTML = Questions.length-1
-    // Create empty List
-    let list = ""
-    // To check Every Question
-    Questions.map((question)=> {
-        if (question !== "") {
-            // Add every <li> At <ul>
-            // 69 stroke removes the TYPE from <li>
-            list += `
-            <li>
-                ${ // Question without TYPE and DATE
-                    question.split(" ").slice(0, -2).join(" ")}
-                <button id="delbtn" style="background-color: transparent; border: none;">
-                    <img src="static/imgs/Icons/Delete-icon.png" alt="">
-                </button>
-            </li>`
-        }
-    })
-
-    completedUI.innerHTML = Completed
-
-    // Shows the list on WebPage
-    document.querySelector("#listQue").innerHTML = list
-    // If there will be to many Questions on the Page
-    document.querySelectorAll("#delbtn").forEach(el => el.addEventListener("click", deleteItem))
-
-    localStorage.setItem('CompletedTimes', JSON.stringify(Completed))
-    localStorage.setItem('TestTimers', JSON.stringify(TimeArray))
-} */
-//
-
-// Delete Item Button
-/* function deleteItem() {
-    let index = Questions.indexOf(this.closest("li").innerText.split(" ").slice(0,3).join(" "))
-    Questions.splice(index, 1)
-    this.closest("li").remove()
-    localStorage.setItem("Questions",JSON.stringify(Questions))
-    
-    if (Questions.length == 1) {
-        Completed = 0
-        ListBlockUI.innerHTML = `
-            <div class="Error-block">
-                <h1>Oops...</h1>
-                <img src="static/imgs/Error-icon.png" alt="Error-icon">
-                <h2>You Don't have any Questions yet</h2>
-            </div>
-        `
-    }
-    Render()
-} */
-
